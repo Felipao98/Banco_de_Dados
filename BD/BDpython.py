a05eb66df8343fe0833ecf59bc163e3cc0af3c13
Felipao98 <80127519+Felipao98@users.noreply.github.com>
2023-03-28 11:51:06 -0300

    Add files via upload

--- BD/BDpython.py
+++ BD/BDpython.py
@@ -14,4 +14,70 @@ def create_server_connection(nome_host, nome_usuario, senha):
     except Error as err:
         print(f"Error: '{err}'")
 
+        def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+        create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+
+ pop_teacher = """
+INSERT INTO teacher VALUES
+(1,  'James', 'Smith', 'ENG', NULL, '1985-04-20', 12345, '+491774553676'),
+(2, 'Stefanie',  'Martin',  'FRA', NULL,  '1970-02-17', 23456, '+491234567890'), 
+(3, 'Steve', 'Wang',  'MAN', 'ENG', '1990-11-12', 34567, '+447840921333'),
+(4, 'Friederike',  'Müller-Rossi', 'DEU', 'ITA', '1987-07-07',  45678, '+492345678901'),
+(5, 'Isobel', 'Ivanova', 'RUS', 'ENG', '1963-05-30',  56789, '+491772635467'),
+(6, 'Niamh', 'Murphy', 'ENG', 'IRI', '1995-09-08',  67890, '+491231231232');
+"""
+
+connection = create_db_connection("localhost", "root", pw, db)
+execute_query(connection, pop_teacher)
+
+connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
+execute_query(connection, create_teacher_table) # Execute our defined query
+
+
+
+UPDATE client 
+SET address = '23 Fingiertweg, 14534 Berlin' 
+WHERE client_id = 101;
+
+
+connection = create_db_connection("localhost", "root", pw, db)
+execute_query(connection, update)
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+DELETE FROM course 
+WHERE course_id = 20;
+
+
+connection = create_db_connection("localhost", "root", pw, db)
+execute_query(connection, delete_course)
+
+
     return connection
