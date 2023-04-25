6d1d3f2fbff65ce5da36baf22b8dea47c5568c1f
Felipao98 <80127519+Felipao98@users.noreply.github.com>
2023-04-25 13:36:38 -0300

    Add files via upload

---
+++ BD/tabelaFIPE/ESPEC_BD.sql
@@ -0,0 +1,40 @@
+CREATE TABLE tipo_veiculo(
+    tipo varchar(10)
+);
+CREATE TABLE modelo(
+    modelo varchar(15),
+    ano int(4),
+    motorizacao varchar(10),
+    combustivel varchar(20),
+    torque varchar(10),
+    potencia int(4),
+    valvulas int(3),
+    cambio varchar(15),
+    codigoFipe varchar(8)
+);
+CREATE TABLE preco(
+    preco_medio decimal(8,2),
+    mes_referencia date,
+    mes_consulta date
+);
+/*
+API em formato JSON
+
+MARCAS DOS VEICULOS
+https://apifipe.com.br/api/carros/token
+
+MODELOS
+https://apifipe.com.br/api/carros/7/token
+
+ANO
+https://apifipe.com.br/api/carros/7/8946/token
+
+VALOR
+https://apifipe.com.br/api/carros/7/8946/2021-1/token
+
+CODIGO FIPE
+https://apifipe.com.br/api/fipe/009258-4/token
+
+CODIGOFIPE + ANO
+https://apifipe.com.br/api/fipe/009258-4/2021-1/token
+*/
